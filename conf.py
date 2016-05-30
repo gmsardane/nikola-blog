71e0cefee9ca05378ee611b2bdb26f91e9b8d2d3
root <root@ip-172-31-27-69.us-west-2.compute.internal>
2016-05-30 02:58:16 +0000

    added gitignore

--- conf.py
+++ conf.py
@@ -139,9 +139,9 @@ NAVIGATION_LINKS = {
             ('/archive.html', 'Archives', 'icon-folder-open-alt'),
             ('/categories/index.html', 'Tags', 'icon-tags'),
             ('/rss.xml', 'RSS', 'icon-rss'),
-            ('https://getnikola.com', 'About me', 'icon-user'),
-            ('https://twitter.com/getnikola', 'My Twitter', 'icon-twitter'),
-            ('https://github.com/getnikola', 'My Github', 'icon-github'),
+            ('http://adalace.org/stories/about-me/', 'About me', 'icon-user'),
+            ('https://twitter.com/lovelyquark', 'My Twitter', 'icon-twitter'),
+            ('https://github.com/gmsardane', 'My Github', 'icon-github'),
         )
 }
 
